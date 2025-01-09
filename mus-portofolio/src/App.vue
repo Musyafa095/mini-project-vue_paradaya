8393a7890f9d555a70c5c6696a24e664498e9537
Musyafa095 <musyafa095@gmail.com>
2025-01-09 17:17:30 +0700

    Done task App.vue

--- mus-portofolio/src/App.vue
+++ mus-portofolio/src/App.vue
@@ -1,11 +1,28 @@
 <template>
-<div class="bg-[#111827] min-h-screen">
-    <Navbar />
-    <HeroSection />
-    <AboutSection />
-    <Skills />
-</div>
-</template>
+    <div class="bg-[#111827] min-h-screen">
+       <Suspense>
+        <template #default>
+           <div>
+              <Navbar />
+              <HeroSection />
+              <ServicesSection />
+              <AboutSection />
+              <Skills />
+              <LatestProjectSection />
+              <ContactSection />
+              <Footer />
+              <BackToTop />
+        </div>
+      </template>
+      <template #fallback>
+        <div class="flex justify-center items-center min-h-screen">
+            <loadingSpinner />
+        </div>
+      </template>
+    </Suspense>
+    </div>
+  </template>
+  
 <script setup>
 import { defineAsyncComponent } from 'vue';
 
@@ -14,4 +31,16 @@ const Navbar = defineAsyncComponent(()=> import ('@/components/NavBar.vue'));
 const HeroSection = defineAsyncComponent(() => import ('@/components/HeroSection.vue'));
 const AboutSection = defineAsyncComponent(() => import ('@/components/AboutSection.vue'));
 const Skills = defineAsyncComponent (() => import ('@/components/Skills.vue'));
+const LatestProjectSection = defineAsyncComponent (() => import ('@/components/LatestProjectSection.vue'));
+const ContactSection =defineAsyncComponent (() => import ('@/components/ContactSection.vue'));
+const Footer = defineAsyncComponent (() => import ('@/components/Footer.vue'));
+const BackToTop = defineAsyncComponent (() => import ('@/components/BackToTop.vue'));
+import LoadingSpinner from './components/LoadingSpinner.vue';
 </script>
+<style>
+
+*{
+  scrollbar-width: thin;
+  scrollbar-color: #111827 #f1f1f1;
+}
+</style>
---
+++ mus-portofolio/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template >
+    <footer class="mt-8 border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+        <div class="container p-12 flex justify-between">
+            <div style="text-shadow: 2px 2px 4px teal, -2px -2px 4px purple;" class="text-white text-2xl font-serif font-bold">MSYAF</div>
+            <p class="text-slate-600">All rights reserved.</p>
+        </div>
+    </footer>
+</template>
+
+<script setup>    
+</script>
